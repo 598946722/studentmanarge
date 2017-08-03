264527a0849bcda858331b222d2d9f6dab77826d
丶摩卡林 <598946722@qq.com>
2017-08-03 16:03:59 +0800

    ver
    1.0

---
+++ js/data.js
@@ -0,0 +1,193 @@
+var title = ['ID', '姓名', '性别', '年龄', '成绩', '手机号'];
+var data = [
+	{
+		'Id': 1,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 2,
+		'Name': 'test',
+		'Gender': 'women',
+		'Age': '18',
+		'Grade': '85',
+		'Tel': '18652000000'
+	},{
+		'Id': 3,
+		'Name': 'test',
+		'Gender': 'women',
+		'Age': '18',
+		'Grade': '90',
+		'Tel': '18652000000'
+	},{
+		'Id': 4,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},	{
+		'Id': 5,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 6,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 7,
+		'Name': 'test',
+		'Gender': 'women',
+		'Age': '2',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 8,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 9,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '15',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 10,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},	{
+		'Id': 11,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '19',
+		'Grade': '80',
+		'Tel': '18652000000'
+	},{
+		'Id': 12,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 13,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '15',
+		'Grade': '85',
+		'Tel': '18652000000'
+	},{
+		'Id': 14,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18655434500'
+	},{
+		'Id': 15,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 16,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},	{
+		'Id': 17,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '19',
+		'Grade': '80',
+		'Tel': '18652000000'
+	},{
+		'Id': 18,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 19,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18655434500'
+	},{
+		'Id': 20,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 21,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},	{
+		'Id': 22,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '19',
+		'Grade': '80',
+		'Tel': '18652000000'
+	},{
+		'Id': 23,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 24,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '15',
+		'Grade': '85',
+		'Tel': '18652000000'
+	},{
+		'Id': 25,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18655434500'
+	},{
+		'Id': 26,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},{
+		'Id': 27,
+		'Name': 'test',
+		'Gender': 'men',
+		'Age': '18',
+		'Grade': '100',
+		'Tel': '18652000000'
+	},
+]
